3dc8bb34622bde968f25f8e6fa32c2a6897f8bae
Franco Leguiza <142357146+LeguizaFranco@users.noreply.github.com>
2025-12-19 08:07:11 -0300

    Update home

--- js/home.js
+++ js/home.js
@@ -423,7 +423,7 @@ function createCarouselSlide(games, slideIndex) {
 }
 
 
-function createGameCard(game, cardIndex = 0) {
+function createGameCard(game, cardIndex = 0, url) {
     if (!game) return '<div class="card gratis"><div class="card-img" style="background: #333;"></div></div>';
 
     const isOwnGame = game.isOwnGame || false;
@@ -449,7 +449,7 @@ function createGameCard(game, cardIndex = 0) {
     return `
         <div class="${cardClass} ${isOwnGame ? 'own-game-card' : ''} animate-in">
             <img src="${game.background_image_low_res}" alt="${game.name}" class="card-img">
-            <button class="play-btn" title="Jugar ${game.name}" ${isOwnGame ? 'onclick="window.location.href=\'game-page.html\'"' : ''}>
+            <button class="play-btn" title="Jugar ${game.name}" ${isOwnGame ? url : ''}>
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M16.6582 9.28638C18.098 10.1862 18.8178 10.6361 19.0647 11.2122C19.2803 11.7152 19.2803 12.2847 19.0647 12.7878C18.8178 13.3638 18.098 13.8137 16.6582 14.7136L9.896 18.94C8.29805 19.9387 7.49907 20.4381 6.83973 20.385C6.26501 20.3388 5.73818 20.0469 5.3944 19.584C5 19.053 5 18.1108 5 16.2264V7.77357C5 5.88919 5 4.94701 5.3944 4.41598C5.73818 3.9531 6.26501 3.66111 6.83973 3.6149C7.49907 3.5619 8.29805 4.06126 9.896 5.05998L16.6582 9.28638Z" stroke="#ffffff" stroke-width="2" stroke-linejoin="round" fill="#ffffff"/>
                 </svg>
@@ -475,11 +475,11 @@ function updateChosenSection(games) {
     if (chosenSlide && games.length >= 5) {
         chosenSlide.innerHTML = `
             <div class="small-column">
-            ${createGameCard(games[0], 0)}
-                ${createGameCard(games[1], 1)}
+            ${createGameCard(games[0], 0, "onclick=\"window.location.href='game-page.html'\"")}
+                ${createGameCard(games[1], 1, "onclick=\"window.location.href='blocka.html'\"")}
                 </div>
                 <div class="large-column">
-                ${createGameCard(games[2], 2)}
+                ${createGameCard(games[2], 2, "onclick=\"window.location.href='flappy.html'\"")}
             </div>
             <div class="small-column">
                 ${createGameCard(games[3], 3)}
